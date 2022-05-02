9d3c259af5ba1cea2155668dfaaff2828aac5352
unknown <sumeraruwen@gmail.com>
2022-05-02 22:46:01 +0530

    Save Item method with ItemDAO

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -32,5 +32,16 @@ return allItems;
         return pstm.executeUpdate()>0;
     }
 
+    public boolean saveItem(ItemDTO itemDTO) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+        pstm.setString(1, itemDTO.getCode());
+        pstm.setString(2, itemDTO.getDescription());
+        pstm.setBigDecimal(3,itemDTO.getUnitPrice());
+        pstm.setInt(4, itemDTO.getQtyOnHand());
+       return pstm.executeUpdate()>0;
+
+    }
+
 
 }
