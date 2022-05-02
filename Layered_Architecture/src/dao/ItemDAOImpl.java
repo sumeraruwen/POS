4047e45fcd660a16166deab15346c023214532ed
unknown <sumeraruwen@gmail.com>
2022-05-03 00:30:02 +0530

    GenerateNewId method with ItemDAOImpl

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -62,6 +62,20 @@ return allItems;
 
     }
 
+    public String generateNewId() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        ResultSet rst = connection.createStatement().executeQuery("SELECT code FROM Item ORDER BY code DESC LIMIT 1;");
+        if (rst.next()) {
+            String id = rst.getString("code");
+            int newItemId = Integer.parseInt(id.replace("I00-", "")) + 1;
+            return String.format("I00-%03d", newItemId);
+        } else {
+            return "I00-001";
+        }
+
+
+    }
+
 
 
 }
